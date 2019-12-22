52eb5967a773c6f6b8d3387a8b97c02776772eab
Nastya Azaza <shcalar_07@mail.ru>
2019-12-22 13:52:30 +0300

    Initial commit

---
+++ src/com/company/task2/SoundTask2n1.java
@@ -0,0 +1,8 @@
+package com.company.task2;
+
+public class SoundTask2n1 {
+    public static void main(String[] args) {
+        double dist = 1100 * 7.2;
+        System.out.print("Расстояние до места вспышки молнии составляет " + dist + " футов");
+    }
+}
